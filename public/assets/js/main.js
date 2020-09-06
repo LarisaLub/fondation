789f7eac84bdfb8ae6e4acce104cfa651fad5d35
Larisa <llubonko@gmail.com>
2020-09-06 17:37:14 +0300

    create footer

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,58 +1,76 @@
-let menuHeader = document.querySelector(".fond");
+ document.querySelector(".fond").addEventListener("click", (event) => {
+     event.preventDefault();
+     let subMenu = document.querySelector("header .sub-menu");
+     subMenu.className += " active";
+ });
+
+ //  let list = document.querySelectorAll("header .sub-menu a");
+ //  for (let i = 0; i < list.length; i++) {
+ //      list[i].addEventListener("click", () => {
+ //          let subMenu = document.querySelector("header .sub-menu");
+ //          subMenu.className = "sub-menu";
+ //      });
+ //  }
+
+ document.querySelector("header .sub-menu").addEventListener("click", () => {
+     document.querySelector(".sub-menu").className = "sub-menu";
+ });
 
 
-const slaides = document.querySelectorAll('.slaider');
-let index = 0;
-for (let i = 0; i < slaides.length; i++) {
-    let circlesList = document.querySelector('.circles');
-    circlesList.innerHTML += `<div class="circle" onclick = "handleClickCircle(${i})"></div>`;
-}
 
-function handleClickCircle(index) {
-    changeIndex(index);
-    changeSlaider();
-}
 
-function changeIndex(i) {
-    index = i;
-}
+ const slaides = document.querySelectorAll('.slaider');
+ let index = 0;
+ for (let i = 0; i < slaides.length; i++) {
+     let circlesList = document.querySelector('.circles');
+     circlesList.innerHTML += `<div class="circle" onclick = "handleClickCircle(${i})"></div>`;
+ }
 
-document.querySelector(".slick-prev").addEventListener("click", () => {
-    const totalSlaides = slaides.length - 1;
-    if (index === 0) {
-        changeIndex(totalSlaides);
-    } else {
-        changeIndex(index - 1);
+ function handleClickCircle(index) {
+     changeIndex(index);
+     changeSlaider();
+ }
 
-    }
+ function changeIndex(i) {
+     index = i;
+ }
 
-    changeSlaider();
-});
+ document.querySelector(".slick-prev").addEventListener("click", () => {
+     const totalSlaides = slaides.length - 1;
+     if (index === 0) {
+         changeIndex(totalSlaides);
+     } else {
+         changeIndex(index - 1);
 
-document.querySelector(".slick-next").addEventListener("click", () => {
-    const totalSlaides = slaides.length - 1;
-    if (index === totalSlaides) {
-        changeIndex(0);
-    } else {
-        changeIndex(index + 1);
+     }
 
-    }
+     changeSlaider();
+ });
 
-    changeSlaider();
-})
+ document.querySelector(".slick-next").addEventListener("click", () => {
+     const totalSlaides = slaides.length - 1;
+     if (index === totalSlaides) {
+         changeIndex(0);
+     } else {
+         changeIndex(index + 1);
 
-function changeSlaider() {
-    let circles = document.querySelectorAll(".circle");
+     }
 
-    for (let i = 0; i < slaides.length; i++) {
-        if (i === index) {
-            slaides[i].className += ' active';
-            circles[i].className += ' active';
-            //console.log(circles[i]);
-        } else {
-            slaides[i].className = 'slaider';
-            circles[i].className = 'circle';
-        }
-    }
+     changeSlaider();
+ })
 
-}
+ function changeSlaider() {
+     let circles = document.querySelectorAll(".circle");
+
+     for (let i = 0; i < slaides.length; i++) {
+         if (i === index) {
+             slaides[i].className += ' active';
+             circles[i].className += ' active';
+             //console.log(circles[i]);
+         } else {
+             slaides[i].className = 'slaider';
+             circles[i].className = 'circle';
+         }
+     }
+
+ }
